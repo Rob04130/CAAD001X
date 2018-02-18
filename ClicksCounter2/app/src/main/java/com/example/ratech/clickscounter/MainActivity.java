c1a988c1383920a5b834402b9f6d9c75a0a1f22c
Robert Abo <robert.abo@hotmail.com>
2018-02-18 20:01:31 +0100

    Adding HelloWorld, ClickCounter and ClickCounter2 to repository

---
+++ ClicksCounter2/app/src/main/java/com/example/ratech/clickscounter/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ratech.clickscounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonReset = (Button) findViewById(R.id.button_reset);
+        Button buttonCount = (Button) findViewById(R.id.button_inc);
+        buttonCount.setOnClickListener(this::submitCount);
+        buttonReset.setOnClickListener(view1 -> {
+            count = 0;
+            display(0);
+        });
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.textView);
+        quantityTextView.setText("" + number);
+    }
+
+    public void submitCount(View view) {
+        count++;
+        display(count);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CAAD001X
+
+This repository is dedicated to the Android projects of the 
+edX_GalileoX_CAAD01X_JFAD course.
+
+There are the following projects:
+1. "HelloWorld": displays "HelloWorld" on the screen (january 2018);
+2. "ClickCounter": a button allow to increment a counter by clicking 
+on it (january 2018);
+3. "ClickCounter2": same idea as the previous project plus another 
+button allowing the user to reset the counter (january 2018)
